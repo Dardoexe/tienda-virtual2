c8b90994f6beff51e652020ddb848211feb64f3a
Dardo Exequiel Gonzalez <dardo135@gmail.com>
2025-06-28 20:27:36 -0300

    solucion de error en formato

--- models/producto.js
+++ models/producto.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const ProductoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  descripcion: { type: String },
-  precio: { type: Number, required: true },
-  categoria: { type: String },
-  stock: { type: Number, default: 0 },
-  fecha_creacion: { type: Date, default: Date.now }
+    id_producto: { type: String, required: true, unique: true }, 
+    nombre: { type: String, required: true },
+    descripcion: { type: String },
+    precio: { type: Number, required: true },
+    categoria: { type: String },
+    stock: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model('Producto', ProductoSchema);
